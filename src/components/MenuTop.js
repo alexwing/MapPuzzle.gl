7924f180c07af6e5cad96001c86b7e1deb3792ca
Alejandro Aranda <alexwing@gmail.com>
2020-12-20 14:20:57 +0100

    Add info windows

--- src/components/MenuTop.js
+++ src/components/MenuTop.js
@@ -6,6 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { Link } from 'react-router-dom'
+import Info from './Info.js';
 
 
 export default class MenuTop extends Component {
@@ -13,6 +14,7 @@ export default class MenuTop extends Component {
     super(props)
     this.state = {
       show: false,
+      showInfo: false,
     }
   }
   render() {
@@ -20,22 +22,40 @@ export default class MenuTop extends Component {
     const { onSelectMap, onResetGame, onFullScreen } = this.props;
     let handleClose = () => {
       this.setState({
-        show: false
+        show: false,
+        showInfo: false
       });
     }
     let handleOK = () => {
       onResetGame();
       this.setState({
-        show: false
+        show: false,
+        showInfo: false
       });
     }
 
     let handleShow = () => {
       this.setState({
-        show: true
+        show: true,
+        showInfo: false
       });
     }
 
+
+    let handleInfo = () => {
+      this.setState({
+        show: false,
+        showInfo: true
+      });
+    }
+
+    let InfoCloseHandle = () => {
+      this.setState({
+        show: false,
+        showInfo: false
+      });
+    }
+    
 
 
     const Puzzles = (
@@ -60,12 +80,13 @@ export default class MenuTop extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           {Puzzles}
           <Form inline>
-
-            <Button id="fullscreen" variant="outline-secondary" onClick={onFullScreen}><span className="navbar-reset-icon"></span></Button>
+            <Button id="fullscreen" variant="outline-secondary" onClick={onFullScreen}><span className="navbar-full-icon"></span></Button>
+            <Button id="info" variant="outline-secondary" onClick={handleInfo}><span className="navbar-info-icon"></span></Button>
             <Button id="reset" variant="outline-primary" onClick={handleShow}>Reset Game</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
+      <Info show={this.state.showInfo} content={this.props.content} InfoClose={InfoCloseHandle}/>
       <Modal show={this.state.show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Do you surrender?</Modal.Title>
@@ -80,6 +101,7 @@ export default class MenuTop extends Component {
           </Button>
         </Modal.Footer>
       </Modal>
+    
     </div>;
   }
 
