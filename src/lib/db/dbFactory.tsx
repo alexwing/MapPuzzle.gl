39a06f4c7efa4c4a729466a592f7adb75d86c768
AlejandroAranda <alejandro.arandamoreno@emeal.nttdata.com>
2022-07-25 10:37:37 +0200

    securize query

--- src/lib/db/dbFactory.tsx
+++ src/lib/db/dbFactory.tsx
@@ -11,10 +11,29 @@ export const dbFactory = {
 
 //function to execute a query,
 export async function query(sql: string): Promise<QueryExecResult[]> {
-  if (ConfigService.backend) {
-    return dbFactory.backend(sql);
+  //securize query
+  try {
+    sql = securizeQuery(sql);
+    if (ConfigService.backend) {
+      return dbFactory.backend(sql);
+    }
+    return dbFactory.sqlite(sql);
   }
-  return dbFactory.sqlite(sql);
+  catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+function securizeQuery(sql: string): string {
+  //disable sql injection
+  sql = sql.replace(/[';]/g, "");
+  //detect INSERT UPDATE DELETE DROP
+  if (sql.toUpperCase().includes("INSERT") || sql.toUpperCase().includes("UPDATE") || sql.toUpperCase().includes("DELETE") || sql.toUpperCase().includes("DROP ")) {
+    sql = "";
+    throw new Error("SQL Injection detected");
+  }
+  return sql;
 }
 
 export default query;
