5753507286e6ad1241010c1a2b32635ccf3c01cd
Alexwing <alexwing@gmail.com>
2022-07-30 21:56:52 +0200

    Update react component to react function menuTop

--- src/components/MenuTop/MenuTop.tsx
+++ src/components/MenuTop/MenuTop.tsx
@@ -1,97 +1,87 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Info from "../Info";
 import PuzzleSelector from "./PuzzleSelector";
 import PuzzleOptions from "./PuzzleOptions";
 import ConfirmDialog from "./ConfirmDialog";
 
-export default class MenuTop extends Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      show: false,
-      showInfo: false,
-    };
+function MenuTop({ 
+  name,
+  onSelectMap,
+  content,
+  onResetGame,
+  onFullScreen,
+  onRefocus,
+  onShowWikiInfo,
+  onShowEditor
+ }: any) {
+  const [show, setShow] = React.useState(false);
+  const [showInfo, setShowInfo] = React.useState(false);
+
+  useEffect(() => {
+    setShow(false);
+    setShowInfo(false);
   }
-  render() {
-    const {
-      onSelectMap,
-      onResetGame,
-      onFullScreen,
-      onRefocus,
-      onShowWikiInfo,
-      onShowEditor,
-    } = this.props;
-    const handleCancel = () => {
-      this.setState({
-        show: false,
-        showInfo: false,
-        showWikiInfo: false,
-        showEditor: false,
-      });
-    };
-    const handleOK = () => {
-      onResetGame();
-      this.setState({
-        show: false,
-        showInfo: false,
-        showWikiInfo: false,
-        showEditor: false,
-      });
-    };
+  , [content]);
 
-    const handleShow = () => {
-      this.setState({
-        show: true,
-        showInfo: false,
-        showWikiInfo: false,
-        showEditor: false,
-      });
-    };
+  const handleCancel = () => {
+    setShow(false);
+    setShowInfo(false);
+  }
 
-    const handleInfo = () => {
-      this.setState({
-        show: false,
-        showInfo: true,
-        showWikiInfo: false,
-      });
-    };
-    return (
-      <React.Fragment>
+  const handleOK = () => {
+    setShow(false);
+    setShowInfo(false);
+    onResetGame();
+  }
+
+  const handleShow = () => {
+    setShow(true);
+    setShowInfo(false);
+  } 
+  const handleShowInfo = () => {
+    setShow(false);
+    setShowInfo(true);
+  }
+
+
+  return (
+    <React.Fragment>
         <Navbar bg="light" expand="lg">
           <Navbar.Brand>
             <img src="./logo192.png" alt="" />
-            {this.props.name}
+            {name}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <PuzzleSelector
-              puzzles={this.props.content}
+              puzzles={content}
               onSelectMap={onSelectMap}
             />
             <PuzzleOptions
               onRefocus={onRefocus}
               onFullScreen={onFullScreen}
-              handleInfo={handleInfo}
+              handleInfo={handleShowInfo}
+              handleShow={handleShow}
               onShowWikiInfo={onShowWikiInfo}
               onShowEditor={onShowEditor}
-              handleShow={handleShow}
             />
           </Navbar.Collapse>
         </Navbar>
         <Info
-          show={this.state.showInfo}
-          content={this.props.content}
+          show={showInfo}
+          content={content}
           InfoClose={handleCancel}
         />
         <ConfirmDialog
-          show={this.state.show}
+          show={show}
           handleCancel={handleCancel}
           handleOK={handleOK}
           title="Do you surrender?"
           message="Click yes, if you want to start a new game"
         />
       </React.Fragment>
-    );
-  }
+  );
 }
+export default MenuTop;
+  
