194417529db2d54c828b211d16ab07316e07bc84
Alexwing <alexwing@gmail.com>
2023-01-14 21:52:28 +0100

    add more langs

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,9 +1,12 @@
-import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import en from './en/translation.json';
-import es from './es/translation.json';
-import fr from './fr/translation.json';
-
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en/translation.json";
+import es from "./es/translation.json";
+import fr from "./fr/translation.json";
+import pt from "./pt/translation.json";
+import de from "./de/translation.json";
+import el from "./el/translation.json";
+import it from "./it/translation.json";
 
 export const resources = {
   en: {
@@ -11,15 +14,27 @@ export const resources = {
   },
   es: {
     translation: es,
-  },   
+  },
   fr: {
     translation: fr,
-  },     
+  },
+  pt: {
+    translation: pt,
+  },
+  de: {
+    translation: de,
+  },
+  el: {
+    translation: el,
+  },
+  it: {
+    translation: it,
+  },
 };
 
 i18next.use(initReactI18next).init({
-  lng: 'en', // if you're using a language detector, do not define the lng option
+  lng: "en", // if you're using a language detector, do not define the lng option
   debug: true,
   resources,
-  fallbackLng: 'en', // Use English as fallback language
+  fallbackLng: "en", // Use English as fallback language
 });
