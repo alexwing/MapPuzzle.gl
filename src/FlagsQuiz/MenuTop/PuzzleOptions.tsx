b6bdd26a01c41e00a6bcaa9a0749f73ca73647dc
Alexwing <alexwing@gmail.com>
2023-11-18 11:51:40 +0100

    Set theme from coockie or system theme

--- src/FlagsQuiz/MenuTop/PuzzleOptions.tsx
+++ src/FlagsQuiz/MenuTop/PuzzleOptions.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { useTranslation } from "react-i18next";
@@ -6,7 +6,9 @@ import ThemeContext from "../../components/ThemeProvider";
 import * as Icon from "react-bootstrap-icons";
 import Tooltip from "react-bootstrap/Tooltip";
 import { OverlayTrigger } from "react-bootstrap";
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
+import { setCookie, getCookie } from "react-simple-cookie-store";
+import { ConfigService } from "../../services/configService";
 
 interface PuzzleOptionsProps {
   onFullScreen: () => void;
@@ -29,15 +31,22 @@ function PuzzleOptions({
 
   const onThemeChange = () => {
     setTheme(theme === "dark" ? "light" : "dark");
+    setCookie("theme", theme === "dark" ? "light" : "dark", ConfigService.cookieDays);
   };
+
+  useEffect(() => {
+    const savedTheme = getCookie("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
 
   const { t } = useTranslation();
   const size = 28;
 
-
-
   const buttons = [
-    { id: "fullscreen",
+    {
+      id: "fullscreen",
       variant: "none",
       onClickHandler: onFullScreen,
       tooltip: t("topMenu.fullscreen"),
@@ -46,9 +55,10 @@ function PuzzleOptions({
       iconColor: "",
       iconClass: "me-2",
       label: t("topMenu.fullscreen"),
-      labelClass: "d-lg-none"
+      labelClass: "d-lg-none",
     },
-    { id: "theme",
+    {
+      id: "theme",
       variant: "none",
       onClickHandler: onThemeChange,
       tooltip: theme === "light" ? t("topMenu.dark") : t("topMenu.light"),
@@ -57,9 +67,10 @@ function PuzzleOptions({
       iconColor: "",
       iconClass: "me-2",
       label: theme === "light" ? t("topMenu.dark") : t("topMenu.light"),
-      labelClass: "d-lg-none"
+      labelClass: "d-lg-none",
     },
-    { id: "info",
+    {
+      id: "info",
       variant: "none",
       onClickHandler: handleInfo,
       tooltip: t("topMenu.about"),
@@ -68,9 +79,10 @@ function PuzzleOptions({
       iconColor: "",
       iconClass: "me-2",
       label: t("topMenu.about"),
-      labelClass: "d-lg-none"
+      labelClass: "d-lg-none",
     },
-    { id: "wiki",
+    {
+      id: "wiki",
       variant: "none",
       onClickHandler: onShowWikiInfoHandler,
       tooltip: t("topMenu.wikiInfo"),
@@ -79,9 +91,10 @@ function PuzzleOptions({
       iconColor: "",
       iconClass: "me-2",
       label: t("topMenu.wikiInfo"),
-      labelClass: "d-lg-none"
+      labelClass: "d-lg-none",
     },
-    { id: "reset",
+    {
+      id: "reset",
       variant: "none",
       onClickHandler: handleShow,
       tooltip: t("topMenu.resetGame"),
@@ -90,39 +103,45 @@ function PuzzleOptions({
       iconColor: "",
       iconClass: "me-2",
       label: t("topMenu.resetGame"),
-      labelClass: "d-lg-none"
+      labelClass: "d-lg-none",
     },
   ];
 
   const overlay = useCallback((button: any) => {
-    return (useMediaQuery({ minWidth: 992 }) ? (
-      <Tooltip id={`tooltip-${button.id}`}>{button.tooltip}</Tooltip>
-    ) : <span></span>) as JSX.Element;
+    return (
+      useMediaQuery({ minWidth: 992 }) ? (
+        <Tooltip id={`tooltip-${button.id}`}>{button.tooltip}</Tooltip>
+      ) : (
+        <span></span>
+      )
+    ) as JSX.Element;
   }, []); // Add dependencies here if any
 
   return (
     <React.Fragment>
       <Form inline>
         {buttons.map((button, index) => (
-                    <OverlayTrigger
+          <OverlayTrigger
             key={index}
             placement="bottom"
             overlay={overlay(button)}
           >
-          <Button
-            key={index}
-            id={button.id}
-            variant={button.variant}
-            onClick={button.onClickHandler}
-          >
-            <span>
-              <button.icon size={button.iconSize} className={button.iconClass} />
-              <span className={button.labelClass}>{button.label}</span>
-            </span>
-          </Button>
+            <Button
+              key={index}
+              id={button.id}
+              variant={button.variant}
+              onClick={button.onClickHandler}
+            >
+              <span>
+                <button.icon
+                  size={button.iconSize}
+                  className={button.iconClass}
+                />
+                <span className={button.labelClass}>{button.label}</span>
+              </span>
+            </Button>
           </OverlayTrigger>
         ))}
-
       </Form>
     </React.Fragment>
   );
--- src/components/MenuTop/PuzzleOptions.tsx
+++ src/components/MenuTop/PuzzleOptions.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { useTranslation } from "react-i18next";
@@ -7,7 +7,8 @@ import * as Icon from "react-bootstrap-icons";
 import Tooltip from "react-bootstrap/Tooltip";
 import { OverlayTrigger } from "react-bootstrap";
 import { useMediaQuery } from 'react-responsive';
-
+import { setCookie, getCookie, removeCookie } from "react-simple-cookie-store";
+import { ConfigService } from "../../services/configService";
 
 interface PuzzleOptionsProps {
   onRefocus: () => void;
@@ -36,7 +37,18 @@ function PuzzleOptions({
   };
   const onThemeChange = () => {
     setTheme(theme === "dark" ? "light" : "dark");
+    setCookie("theme", theme === "dark" ? "light" : "dark", ConfigService.cookieDays);
   };
+
+  useEffect(() => {
+    const savedTheme = getCookie("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    } else {
+      const systemTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+      setTheme(systemTheme);
+    }
+  }, []);
 
   const { t } = useTranslation();
   const size = 28;
