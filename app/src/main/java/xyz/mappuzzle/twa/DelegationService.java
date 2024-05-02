97c3afbe246f77671e2d9b246cea67fa150172a4
Alexwing <alexwing@gmail.com>
2024-05-02 23:06:21 +0200

    Add android apk config

---
+++ app/src/main/java/xyz/mappuzzle/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package xyz.mappuzzle.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
