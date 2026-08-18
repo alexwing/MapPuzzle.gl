d4833ac3e8b57e268b77e0190824b17103125a70
Alexwing <alexwing@gmail.com>
2026-08-19 00:33:05 +0200

    style: liquid glass victory cards and luminous foreground fireworks with organic distribution

--- apps/game/src/lib/Fireworks.tsx
+++ apps/game/src/lib/Fireworks.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Fireworks.scss";
+
 /**
  * Fireworks
- * Pure CSS Fireworks
+ * Pure GPU-accelerated CSS Fireworks with vivid luminous particles
  *
  * @author Port to React: Alejandro Aranda (github.com/alexwing)
  * @author Script CSS: Eddie Lin (https://codepen.io/yshlin)
- *
  */
-function Fireworks () : JSX.Element {
+function Fireworks(): JSX.Element {
   return (
     <React.Fragment>
-      <div className="pyro">
+      <div className="pyro" aria-hidden="true">
         <div className="before"></div>
         <div className="after"></div>
+        <div className="sparkles1"></div>
+        <div className="sparkles2"></div>
       </div>
     </React.Fragment>
   );
 }
+
 export default Fireworks;
